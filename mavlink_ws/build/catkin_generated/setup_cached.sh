6487f0350e1a16f58b5b1d5b1dce16c7a6849316
chickonice <natalie.afonina@gmail.com>
2016-07-01 12:11:44 -0700

    compiled mavlink package

---
+++ mavlink_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/spacecat/AutonomousFlight/AutonomousFlight/mavlink_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/spacecat/AutonomousFlight/AutonomousFlight/mavlink_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/spacecat/AutonomousFlight/AutonomousFlight/mavlink_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/spacecat/AutonomousFlight/AutonomousFlight/mavlink_ws/src:$ROS_PACKAGE_PATH"
